3367ef25a351f5293f0a1628fa54759153a8f950
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-02-25 10:21:32 +0300

    referee

---
+++ verification/src/tests.py
@@ -0,0 +1,77 @@
+TESTS = {
+    "Level_1": [
+        {
+            "input": 15,
+            "answer": "Fizz Buzz"
+        },
+        {
+            "input": 6,
+            "answer": "Fizz"
+        },
+        {
+            "input": 10,
+            "answer": "Buzz"
+        },
+        {
+            "input": 7,
+            "answer": "7"
+        },
+        {
+            "input": 1000,
+            "answer": "Buzz"
+        },
+        {
+            "input": 1,
+            "answer": "1"
+        },
+        {
+            "input": 990,
+            "answer": "Fizz Buzz"
+        },
+        {
+            "input": 45,
+            "answer": "Fizz Buzz"
+        },
+
+        {
+            "input": 46,
+            "answer": "46"
+        },
+
+        {
+            "input": 47,
+            "answer": "47"
+        },
+
+        {
+            "input": 48,
+            "answer": "Fizz"
+        },
+
+        {
+            "input": 49,
+            "answer": "49"
+        },
+
+        {
+            "input": 50,
+            "answer": "Buzz"
+        },
+        {
+            "input": 999,
+            "answer": "Fizz"
+        },
+
+        {
+            "input": 1000,
+            "answer": "Buzz"
+        },
+
+        {
+            "input": 989,
+            "answer": "989"
+        },
+
+
+    ]
+}
